a5ccf5814ccaf959a4b4336e62e18b222c84a085
Ryan Kubik <rmkubik@me.com>
2023-06-18 13:14:32 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+const App = () => {
+  return <h1>Hello world!</h1>;
+};
+
+const root = document.getElementById("root");
+ReactDOM.render(<App />, root);
